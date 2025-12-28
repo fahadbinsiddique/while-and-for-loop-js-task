7c29b7091221748c91659f77540439d9a3b365da
fahadbinsiddique <fahad.bin.siddique345@gmail.com>
2025-12-28 19:38:00 +0600

    udate

--- arrayTask.js
+++ arrayTask.js
@@ -10,3 +10,18 @@ let numb1 = ["fruit-1", "fruit-2", "fruit-3", "fruit-4", "fruit-5"];
 console.log(numb1[3]);
 numb1[2] = "jambura";
 console.log(numb1);
+
+/*2. Add or remove elements 
+Declare an array of 3 tourist destinations 
+Add a new tourist destination to your tourist array 
+Add two more to your array 
+Remove the last tourist destination you have added 
+display the final array as output */
+
+let numb2 = ["tourist-1", "tourist-2", "tourist-3"];
+numb2.push("tourist-new");
+console.log("adding a new tourist destination", numb2);
+numb2.push("tourist-new-1", "tourist-new-2");
+console.log("adding two more", numb2);
+numb2.pop();
+console.log("remove and final", numb2);
