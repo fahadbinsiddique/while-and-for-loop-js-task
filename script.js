2a1a719e9a783f8573403e3b3ac0ab3daa54019f
fahadbinsiddique <fahad.bin.siddique345@gmail.com>
2026-01-02 17:29:47 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// While Loop:
+
+// Problem-1:
+// I will invest at least 6 hrs every single day for next 60 days! this
+// message 60 times. So display this.
+
+let numb1 = 1;
+while (numb1 <= 60) {
+  // console.log(
+  //   numb1,
+  //   ":I will invest at least 6 hrs every single day for next 60 days!"
+  // );
+
+  numb1++;
+}
