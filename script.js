af7c0dc9a08c57f6e465d96907d421a482481992
fahadbinsiddique <fahad.bin.siddique345@gmail.com>
2026-01-03 17:44:32 +0600

    try

--- script.js
+++ script.js
@@ -6,10 +6,33 @@
 
 let numb1 = 1;
 while (numb1 <= 60) {
-  // console.log(
-  //   numb1,
-  //   ":I will invest at least 6 hrs every single day for next 60 days!"
-  // );
+  console.log(
+    numb1,
+    ":I will invest at least 6 hrs every single day for next 60 days!"
+  );
 
   numb1++;
+}
+
+/*Problem-2 : 
+Subtask-1: 
+Find all the odd numbers from 61 to 100. */
+
+let numb2 = 61;
+while (numb2 <= 100) {
+  if (numb2 % 2 === 1) {
+    console.log(numb2);
+  }
+  numb2++;
+}
+
+// Subtask-2:
+// Find all the even numbers from 78 to 98.
+
+let numb3 = 78;
+while (numb3 <= 98) {
+  if (numb3 % 2 === 0) {
+    console.log(numb3);
+  }
+  numb3++;
 }
